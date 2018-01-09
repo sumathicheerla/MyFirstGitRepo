a534086fdabd328401767df4e35e0409f432b809
sumathi <sumathisanthosh.cheerla@gmail.com>
2018-01-09 09:37:27 +0530

    jenkins testing......

--- BookMyShow/src/com/sumathi/bookmyshow/BookMyShowController.java
+++ BookMyShow/src/com/sumathi/bookmyshow/BookMyShowController.java
@@ -36,6 +36,7 @@ public class BookMyShowController extends HttpServlet {
 	{
 		System.out.println("git test.........");
 		System.out.println("Git Test2..........");
+		System.out.println("testing jenkins");
 		/*String Log4jLocation=config.getInitParameter("log4j-properties-location");
 		ServletContext context=config.getServletContext();
 		if(Log4jLocation==null)
